4e55417ca47df5a33ce2c43dcf45630aba370160
Morteza Taghaddomi Masoumi <mortezataqaddomi99@gmail.com>
2019-01-09 02:24:11 +0330

    calculate popularity method added

---
+++ python/shitty_py.py
@@ -0,0 +1,8 @@
+def popularity():
+    print("I've developed a simple algorithm for determining if a girl likes me:")
+    print("1) NO")
+    print("\n")
+    print("Not bad, at least it has O(1) time and space complexity :D")
+    
+    
+popularity()
