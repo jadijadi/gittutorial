b2ebe4c3ff264f9a93dabcedac19c2ef4bec4ffb
venowm <43109361+venowm@users.noreply.github.com>
2020-06-12 19:46:53 +0430

    create a new python script
    generate strong password using this script.
    its only 7 line 😄 :

---
+++ python/password generator.py
@@ -0,0 +1,7 @@
+import string
+from random import *
+characters = string.ascii_letters + string.punctuation  + string.digits
+password =  "".join(choice(characters) for x in range(randint(8, 16)))
+print("this is your password:")
+print (password)
+print("Good Luck")
