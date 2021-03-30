054c98bfca0a228037fd80296f8e8333dcf2f8cf
hosyn <itshosyn@protonmail.com>
2021-03-31 01:42:53 +0430

    Description has been corrected

--- python/switch_two_variables.py
+++ python/switch_two_variables.py
@@ -1,4 +1,4 @@
-# Coud you switch two variables without an extra variable ?'
+# Could you switch two variables without an extra variable ?'
 # Challange accepted!
 a, b = 12, 10
 print('a is {}, b is {}'.format(a, b))
