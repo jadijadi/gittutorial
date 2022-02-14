d68997419d4516f59590d466fc472f80e6506337
Sajjad <62008800+Sajjad-Khoshdooni@users.noreply.github.com>
2022-02-14 20:37:41 +0330

    4 variable swapper added

--- python/switch_two_variables.py
+++ python/switch_two_variables.py
@@ -4,3 +4,11 @@ a, b = 12, 10
 print('a is {}, b is {}'.format(a, b))
 a,b = b,a
 print('After switching:\na is {}, b is {}'.format(a, b))
+
+
+
+#Lets swith four variable🖖🏻
+a, b, c, d = 12, 10, 8, 6
+print('a is {}, b is {}, c is {}, d is {}'.format(a, b, c ,d))
+a,b,c,d = d,c,b,a
+print('After switching:\na is {}, b is {}, c is {}, d is {}'.format(a, b, c, d))
