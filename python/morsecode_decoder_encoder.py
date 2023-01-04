f531e4f9b579974282c48c0b3b0e0716969676e8
Torham <torhamdev@gmail.com>
2023-01-04 20:08:44 +0330

    Add function encode_morse_code
    Using this function, we encode user english text to morse code

--- python/morsecode_decoder_encoder.py
+++ python/morsecode_decoder_encoder.py
@@ -19,3 +19,25 @@ def get_user_selection() -> int:
     user_selection = int(input("Enter your choice[1 or 2]:"))
 
     return user_selection
+
+
+def encode_morse_code(text: str) -> str:
+    """
+    Convert the user's English text to Morse code.
+
+    param : text : user english text
+
+    retrun : morse code
+    """
+
+    morse_code = str()
+
+    for char in text:
+
+        if char not in ENCODE_DICT:
+            print(f"I cannot encode the {char} character :(")
+            quit()
+
+        morse_code += ENCODE_DICT[char]
+
+    return char
